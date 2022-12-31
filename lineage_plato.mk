58442699bd0d00b454e61d4b08a5ba8e41a4e2e4
Arian <arian.kulmer@web.de>
2022-12-31 16:09:26 +0100

    plato: Set stock TARGET_DEVICE and TARGET_PRODUCT
    Change-Id: Ib40dd59a0e8e4df6ade54a6a7a7434bfb7a06409

--- lineage_plato.mk
+++ lineage_plato.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -20,7 +20,12 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 22071212AG
 
+PRODUCT_SYSTEM_NAME := plato_global
+PRODUCT_SYSTEM_DEVICE := plato
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="plato_global-user 12 SP1A.210812.016 V14.0.7.0.ULQMIXM release-keys"
+    PRIVATE_BUILD_DESC="plato_global-user 12 SP1A.210812.016 V14.0.7.0.ULQMIXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 BUILD_FINGERPRINT := Xiaomi/plato_global/plato:12/SP1A.210812.016/V14.0.7.0.ULQMIXM:user/release-keys
